34195b41ccf6a664a2661d293f7a1dff524202a0
Luka_Radulovic <luka.radulovic.011@gmail.com>
2022-06-20 11:15:13 +0200

    Refactored methods, added proper Build ID in parameters

--- Program.cs
+++ Program.cs
@@ -17,14 +17,17 @@ namespace APICallerAppConsole
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // Deserailize first JSON list of branches, get json branch model
-            var stringToDeserialize = new Program().GetBranchesList();
-            var jsonBranchModelList = JsonConvert.DeserializeObject<List<Root>>(stringToDeserialize);
-            var branchNamesList = jsonBranchModelList.Select(x => x.Branch.Name).ToList();
-            var branchNameLastBuild = string.Empty;
-            var buildStatus = string.Empty;
+            ExecuteAPICommunication();
+        }
 
-            var buildLogFromBetaBranch = string.Empty;
+        private static void ExecuteAPICommunication()
+        {
+            WebAPIClient webAPIClient;
+            List<Root> jsonBranchModelList;
+            List<string> branchNamesList;
+            string branchNameLastBuild, buildStatus;
+            AcquireAPICommunicationParameters(out webAPIClient, out jsonBranchModelList, out branchNamesList, out branchNameLastBuild, out buildStatus);
+
 
             // Deserailize and get latest build info after posting
             var stringToDeserializeUpdated = string.Empty;
@@ -32,40 +35,68 @@ namespace APICallerAppConsole
 
             foreach (var branch in branchNamesList)
             {
-                branchNameLastBuild = jsonBranchModelList.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.BuildNumber).FirstOrDefault();
-                buildStatus = jsonBranchModelList.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.Status).FirstOrDefault();
+                GetCurrentBranchBuildInfo(jsonBranchModelList, out branchNameLastBuild, out buildStatus, branch);
 
-                // Create build
-                Console.WriteLine($"Calling Post method that creates a new build on {branch} branch");
-                PostCreateBuild(branch);
+                CreateBuildPOSTMethod(branch);
 
                 Console.WriteLine("Wait 2 minutes for build response.");
-                Thread.Sleep(180000);
+                Thread.Sleep(300000);
 
-                // Update build info from branch
+                PrintUpdatedBranchBuildInfo(webAPIClient, out branchNameLastBuild, out buildStatus, out stringToDeserializeUpdated, out jsonBranchModelListUpdated, branch);
 
-                stringToDeserializeUpdated = new Program().GetBranchesList();
-
-                jsonBranchModelListUpdated = JsonConvert.DeserializeObject<List<Root>>(stringToDeserializeUpdated);
-
-                branchNameLastBuild = jsonBranchModelListUpdated.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.BuildNumber).FirstOrDefault();
-
-                buildStatus = jsonBranchModelListUpdated.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.Status).FirstOrDefault();
-                if (buildStatus.Contains("Completed"))
-                {
-                    Console.WriteLine($"Build ID: {branchNameLastBuild} for Branch: {branch} status: {buildStatus} \nBuild log link: {GetBuildLogsLink(branchNameLastBuild)}\n");
-                }
-                else
-                {
-                    Thread.Sleep(125000);
-                    Console.WriteLine($"Build ID: {branchNameLastBuild} for Branch: {branch} status: {buildStatus} \nBuild log link: {GetBuildLogsLink(branchNameLastBuild)}\n");
-                }
-                
             }
         }
 
+        private static void CreateBuildPOSTMethod(string branch)
+        {
+            // Create build
+            Console.WriteLine($"Calling Post method that creates a new build on {branch} branch");
+            WebAPIClient.PostCreateBuild(branch);
+        }
 
-        private static void PostCreateBuild(string branchName)
+        private static void PrintUpdatedBranchBuildInfo(WebAPIClient webAPIClient, out string branchNameLastBuild, out string buildStatus, out string stringToDeserializeUpdated, out List<Root> jsonBranchModelListUpdated, string branch)
+        {
+            // Update build info from branch
+
+            stringToDeserializeUpdated = webAPIClient.GetBranchesList();
+
+            jsonBranchModelListUpdated = JsonConvert.DeserializeObject<List<Root>>(stringToDeserializeUpdated);
+
+            branchNameLastBuild = jsonBranchModelListUpdated.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.BuildNumber).FirstOrDefault();
+
+            buildStatus = jsonBranchModelListUpdated.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.Status).FirstOrDefault();
+
+            if (buildStatus.Contains("Completed"))
+            {
+                Console.WriteLine($"Build ID: {branchNameLastBuild} for Branch: {branch} status: {buildStatus} \nBuild log link: {WebAPIClient.GetBuildLogsLink(branchNameLastBuild)}\n");
+            }
+            else
+            {
+                Thread.Sleep(125000);
+                Console.WriteLine($"Build ID: {branchNameLastBuild} for Branch: {branch} status: {buildStatus} \nBuild log link: {WebAPIClient.GetBuildLogsLink(branchNameLastBuild)}\n");
+            }
+        }
+
+        private static void GetCurrentBranchBuildInfo(List<Root> jsonBranchModelList, out string branchNameLastBuild, out string buildStatus, string branch)
+        {
+            branchNameLastBuild = jsonBranchModelList.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.BuildNumber).FirstOrDefault();
+            buildStatus = jsonBranchModelList.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.Status).FirstOrDefault();
+        }
+
+        private static void AcquireAPICommunicationParameters(out WebAPIClient webAPIClient, out List<Root> jsonBranchModelList, out List<string> branchNamesList, out string branchNameLastBuild, out string buildStatus)
+        {
+            // Deserailize first JSON list of branches, get json branch models and its values
+            webAPIClient = new WebAPIClient();
+            var stringToDeserialize = webAPIClient.GetBranchesList();
+            jsonBranchModelList = JsonConvert.DeserializeObject<List<Root>>(stringToDeserialize);
+            branchNamesList = jsonBranchModelList.Select(x => x.Branch.Name).ToList();
+            branchNameLastBuild = string.Empty;
+            buildStatus = string.Empty;
+        }
+    }
+    public class WebAPIClient
+    {
+        public static void PostCreateBuild(string branchName)
         {
             // Post method
             string postBuildUrl = String.Format($"https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/branches/{branchName}/builds");
@@ -94,7 +125,7 @@ namespace APICallerAppConsole
             Console.WriteLine("Create build initiated.");
         }
 
-        private string GetBranchesList()
+        public string GetBranchesList()
         {
             // Get list of branches method
             string userUrlPage = String.Format("https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/branches");
@@ -119,11 +150,10 @@ namespace APICallerAppConsole
             }
             return branchListResult;
         }
-        private static string GetBuildLogsLink(string buildID)
+        public static string GetBuildLogsLink(string buildID)
         {
-            return string.Format($"https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/builds/{buildID}/logs?build_id=17&owner_name=lukaradulovic1&app_name=Xamarin-antistress-app");
+            return string.Format($"https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/builds/{buildID}/logs?build_id={buildID}&owner_name=lukaradulovic1&app_name=Xamarin-antistress-app");
         }
-
     }
 
 }
